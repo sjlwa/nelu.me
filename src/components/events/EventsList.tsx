fcc8993ee7635718a9e14841ac865af4d46cc83a
sjlwa <ivansilva.me@gmail.com>
2025-06-30 03:06:17 +0000

    feat: show message when no events are available.

--- src/components/events/EventsList.tsx
+++ src/components/events/EventsList.tsx
@@ -11,7 +11,7 @@ export default function EventsList(props: Props) {
     const { events, loading } = props;
 
     if (loading.value) return (
-        <div class="bg-dark p-6 flex flex-row justify-between rounded-sm text-light">
+        <div class="font-semibold text-light/60 rounded-3xl text-center p-2 mx-6 italic">
             Cargando eventos ...
         </div>
     );
@@ -20,9 +20,13 @@ export default function EventsList(props: Props) {
         <div class="flex flex-col gap-1">
             <div class="flex flex-col gap-1 @container">
                 {
-                    events.value.map((event: NeluEventState, index: number) => (
-                        <EventCard key={index} event={event} />
-                    ))
+                    events.value.length === 0 ?
+                        <div class="font-semibold rounde text-center text-pink-200 p-2 mx-6">
+                            Aún no hay eventos disponibles.
+                        </div>
+                        : events.value.map((event: NeluEventState, index: number) => (
+                            <EventCard key={index} event={event} />
+                        ))
                 }
             </div>
         </div>
